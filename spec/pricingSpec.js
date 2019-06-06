1467dccb119f11a3b46c7e9e3bc7a2755365938b
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-06-06 12:27:45 +0100

    Error 400, when api key is correct

--- spec/pricingSpec.js
+++ spec/pricingSpec.js
@@ -10,54 +10,56 @@ describe('Pricing', function () {
     pricing = new Pricing(order, prices)
   })
 
-  it ('can access order info', function () {
+  it('can access order info', function () {
     expect(pricing.order.order.id).toEqual(12345)
   })
 
-  it ('can access pricing info', function () {
+  it('can access pricing info', function () {
     expect(pricing.priceDetails.prices[0].product_id).toEqual(1)
     expect(pricing.priceDetails.prices[0].price).toEqual(599)
     expect(pricing.priceDetails.prices[0].vat_band).toEqual('standard')
   })
 
-  it ('initializes with cost at zero', function () {
+  it('initializes with cost at zero', function () {
     expect(pricing.cost).toEqual(0)
   })
 
-  it ('has access to full details of all items in basket', function () {
+  it('has access to full details of all items in basket', function () {
     expect(pricing.items[0]['product_id']).toEqual(1)
     expect(pricing.items[0]['quantity']).toEqual(1)
   })
 
-  it ('adds up the cost of all items in basket', function () {
-    pricing.createReceipt()
-    expect(pricing.cost).toEqual(2099)
-    expect(pricing.vat).toEqual(120)
-  })
+  describe('#createReceipt', function () {
+    beforeEach(function () {
+      pricing.createReceipt()
+    })
 
-  it ('calculates price and VAT for each item', function () {
-    pricing.createReceipt()
-    expect(pricing.receipt).toEqual(
-      [
+    it('adds up the cost of all items in basket', function () {
+      expect(pricing.cost).toEqual(2099)
+      expect(pricing.vat).toEqual(120)
+    })
+
+    it('calculates price and VAT for each item', function () {
+      expect(pricing.receipt.order[0]).toEqual(
         {
           product_id: 1,
           quantity: 1,
           price: 599,
           vat: 120
-        },
-        {
-          product_id: 2,
-          quantity: 5,
-          price: 1250,
-          vat: 0
-        },
-        {
-          product_id: 3,
-          quantity: 1,
-          price: 250,
-          vat: 0
         }
-      ]
-    )
+      )
+      expect(pricing.receipt.total).toEqual(2219)
+    })
+  })
+
+  describe('#convertCurrency', function () {
+    beforeEach(function () {
+      pricing.createReceipt()
+    })
+
+    it('can convert currency to USD', function () {
+      pricing.convertCurrency('USD')
+      expect(pricing.receipt.total).toEqual(2819)
+    })
   })
 })
--- src/pricing.js
+++ src/pricing.js
@@ -1,4 +1,7 @@
-function Pricing (order, priceDetails) {
+const fetch = require('node-fetch')
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+
+function Pricing(order, priceDetails) {
   this.order = order
   this.priceDetails = priceDetails
   this.products = priceDetails.prices
@@ -6,30 +9,69 @@ function Pricing (order, priceDetails) {
   this.cost = 0
   this.vat = 0
   this.items = order.order.items
-  this.receipt = []
+  this.receipt = {
+    order: [],
+    total: 0
+  }
 }
 
-Pricing.prototype.createReceipt = function () {
+Pricing.prototype.createReceipt = function() {
   var receipt = this.receipt
   var products = this.products
   var vatRates = this.vatRates
   var basketCost = 0
   var basketVat = 0
-  this.items.forEach(function (item) {
+  this.items.forEach(function(item) {
     var receiptItem = item
-    products.forEach(function (product) {
+    products.forEach(function(product) {
       if (item.product_id === product.product_id) {
         receiptItem.price = product.price * item.quantity
-        receiptItem.vat = Math.round(receiptItem.price * vatRates[product.vat_band])
+        receiptItem.vat = Math.round(
+          receiptItem.price * vatRates[product.vat_band]
+        )
         var itemCost = product.price * item.quantity
         basketCost += itemCost
         basketVat += itemCost * vatRates[product.vat_band]
       }
     })
-    receipt.push(receiptItem)
+    receipt.order.push(receiptItem)
   })
-  this.cost += basketCost
-  this.vat += Math.round(basketVat)
+  this.calculateCostAndVat(basketCost, basketVat)
+  this.totalCostWithVat()
+}
+
+Pricing.prototype.calculateCostAndVat = function(cost, vat) {
+  this.cost += cost
+  this.vat += Math.round(vat)
+}
+
+Pricing.prototype.totalCostWithVat = function() {
+  this.receipt.total += this.cost + this.vat
+}
+
+Pricing.prototype.convertCurrency = function(currency) {
+  var domain = 'https://free.currconv.com/api/v7/'
+  var conversion = `convert?q=GBP_${currency}`
+  var compact = '&compact=ultra'
+  var api = '&apiKey=a179f97a2c6c58238742'
+  var url = domain + conversion + compact + api
+  console.log(url)
+
+  var request = new XMLHttpRequest()
+
+  request.open('GET', url, true)
+
+  request.onload = function () {
+    var data = JSON.parse(this.response)
+    console.log(data)
+
+    if (request.status >= 200 && request.status < 400) {
+      console.log(data)
+    } else {
+      console.log('error')
+    }
+  }
+  request.send()
 }
 
 module.exports = Pricing
